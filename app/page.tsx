3b5f676e29f7efabd07a0a3c80a34ee3c9413721
Fabrizio <fabrizioterribile@gmail.com>
2026-03-30 02:29:03 -0400

    Features added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Landing from "./components/Landing";
+import Features from "./components/Features";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Landing/>
+      <Features/>
     </>
   );
 }
